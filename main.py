c05c774aeba2a68cfc1dc138c15f6a18325ff0d3
Mafouz <mafouzsanny@gmail.com>
2025-09-29 02:06:01 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+from ui import home, dashboard
+
+st.set_page_config(page_title="FitTrack", layout="wide")
+
+page = st.sidebar.selectbox("Navigation", ["Accueil", "Tableau de bord"])
+
+if page == "Accueil":
+    home.show()
+elif page == "Tableau de bord":
+    dashboard.show()
