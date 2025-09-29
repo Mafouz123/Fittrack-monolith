6a78fb46a2200572df9ea1843cc2091100b94db2
Mafouz <mafouzsanny@gmail.com>
2025-09-29 17:28:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-from flask import Flask
-import os
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Bienvenue sur l'API de Mafouz"
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
-
 import streamlit as st
 from ui import home, dashboard
 
@@ -22,4 +9,4 @@ if page == "Accueil":
     home.show()
 elif page == "Tableau de bord":
     dashboard.show()
-import os
+
